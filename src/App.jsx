fe41309c183536fdd664794fcb85901d63dc7169
matt-314 <matt.sosa314@outlook.com>
2023-09-30 23:51:57 +0000

    feat: added testMode attribute to the App widget so we can render the different test pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+import AnchorTestPage from './_tests/AnchorTestPage';
+
 import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
 
 import styles from './App.module.css';
 
-function App() {
-  return (
-    <>
-      <header className={`${styles.contentPadding} ${styles.header}`}>
-        <Navbar />
-      </header>
-      <main className={styles.contentPadding}>
-        <Main />
-      </main>
-    </>
-  );
+function getTestPage(testMode) {
+  switch (testMode.page) {
+    case 'anchor':
+      return (
+        <main className={styles.contentPadding}>
+          <AnchorTestPage />
+        </main>
+      );
+    default:
+      return null;
+  }
 }
+
+function App({ testMode }) {
+  return testMode
+    ? getTestPage(testMode)
+    : (
+      <>
+        <header className={`${styles.contentPadding} ${styles.header}`}>
+          <Navbar />
+        </header>
+        <main className={styles.contentPadding}>
+          <Main />
+        </main>
+      </>
+    );
+}
+
+App.propTypes = {
+  testMode: PropTypes.objectOf({
+    page: PropTypes.oneOf(['anchor']),
+  }),
+};
+
+App.defaultProps = {
+  testMode: undefined,
+};
 
 export default App;
