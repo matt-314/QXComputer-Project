93cecc6a08983d5fa96f1813d529e8a34526c843
matt-314 <matt.sosa314@outlook.com>
2023-10-06 00:29:22 +0000

    feat: moved the Cart svg icon to a react widget

--- src/components/Navbar/NavbarCart.jsx
+++ src/components/Navbar/NavbarCart.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import CartIcon from '../../assets/icons/CartIcon';
+
 import styles from './NavbarCart.module.css';
 
 function NavbarCart({ cartItems }) {
   return (
     <button type="button" className={styles.cart} aria-label={`Cart with ${cartItems} item`}>
-      <img className={styles.cart__image} src="/images/cart.svg" alt="" />
+      <CartIcon color="primary" height={28} contrast />
       <span className={`${styles.cart__badge} ${styles['cart__badge--color']}`}>{cartItems}</span>
     </button>
   );
