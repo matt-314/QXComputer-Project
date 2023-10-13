87dee2d22bf62c1cc943122404bb233c121a1c08
matt-314 <matt.sosa314@outlook.com>
2023-10-13 13:39:30 +0000

    feat: using new Button size for the cart button in the navbar

--- src/components/Navbar/NavbarCart.jsx
+++ src/components/Navbar/NavbarCart.jsx
@@ -7,10 +7,10 @@ import styles from './NavbarCart.module.css';
 
 function NavbarCart({ cartItems }) {
   return (
-    <>
-      <Button icon="cart" size="md" color="primary" contrast iconOnly>Cart</Button>
+    <div className={styles.cart}>
+      <Button icon="cart" size="xl" color="primary" contrast iconOnly>Cart</Button>
       <span className={`${styles.cart__badge} ${styles['cart__badge--color']}`}>{cartItems}</span>
-    </>
+    </div>
   );
 }
 
