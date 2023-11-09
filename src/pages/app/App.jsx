85d8ad187c7f39fb8c3e78f24363f9584ba0fe83
matt-314 <matt.sosa314@outlook.com>
2023-11-09 22:48:39 +0000

    feat: enclose the app into the cart context

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -3,18 +3,20 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from '../../components/Navbar/Navbar';
 
+import { CartProvider } from '../../contexts/Cart/CartContext';
+
 import styles from './App.module.css';
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <header className={styles.header}>
         <Navbar />
       </header>
       <main>
         <Outlet />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
